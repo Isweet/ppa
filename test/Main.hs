edc79303926c9bffc9f664bc20b2e2dc95cf7909
Ian Sweet <iannsweet@gmail.com>
2015-12-27 18:15:14 -0500

    Better tests, should really be using HUnit

--- test/Main.hs
+++ test/Main.hs
@@ -1,16 +1,37 @@
 import PPA.While
 
+import qualified Data.Set as Set
+import qualified Data.List as List
+
 printStmt :: Stmt -> IO ()
 printStmt = putStrLn . showL
+
+printSet :: (Show a) => Set.Set a -> IO ()
+printSet s = putStrLn $ "{" ++ es ++ "}"
+    where es = concat $ List.intersperse ", " $ map show $ Set.toList s
+
+printCase :: String -> String -> IO ()
+printCase name prog = do
+    let s = parse prog
+    putStr $ name ++ ": "
+    printStmt s
+    putStr $ "labels(" ++ name ++ ") = "
+    printSet $ wlabels s
+    putStr $ "init(" ++ name ++ ")   = "
+    print $ winit s
+    putStr $ "flow(" ++ name ++ ")   = "
+    printSet $ wflow s
+    putStr $ "final(" ++ name ++ ")  = "
+    printSet $ wfinal s
 
 power :: String
 power = "z:=1; while x>0 do (z:=z*y; x:=x-1)"
 
+factorial :: String
+factorial = "y:=x; z:=1; while y>1 do (z:=z*y; y:=y-1); y:=0"
+
 main :: IO ()
 main = do
-    let s = parse power
-    print $ winit s
-    print $ wfinal s
-    print $ wblocks s
-    print $ wlabels s
-    print $ wflow s
+    printCase "power" power
+    putStrLn ""
+    printCase "factorial" factorial
