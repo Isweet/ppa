52fd3fe9cbc3359a7dff753fb57a4c0a45f912e7
Ian Sweet <iannsweet@gmail.com>
2015-12-27 20:11:23 -0500

    prettier test program strings

--- test/Main.hs
+++ test/Main.hs
@@ -29,16 +29,42 @@ printCase name prog = do
 -- Progs
 
 ifBasic :: String
-ifBasic = "if (a > 0 || a < 0) then x := -2 else skip"
+ifBasic =
+    "   if (a > 0 || a < 0) then \
+      \     (x := -2)            \
+      \ else                     \
+      \     (skip)"
 
 ifTwice :: String
-ifTwice = "if (a > 0 || a < 0) then (x := -2) else (skip); if (b < 5) then (y := 1) else (skip)"
+ifTwice = 
+    "   if (a > 0 || a < 0) then \ 
+      \     (x := -2)            \ 
+      \ else                     \ 
+      \     (skip);              \ 
+      \ if (b < 5) then          \
+      \     (y := 1)             \
+      \ else                     \
+      \     (skip)"
 
+{- y^x
+ - result stored in z -}
 power :: String
-power = "z:=1; while x>0 do (z:=z*y; x:=x-1)"
+power = 
+    "   z := 1;             \
+      \ while (x > 0) do    \
+      \     (z := z * y;    \
+      \      x := x - 1)"
 
+{- x!
+ - result stored in z -}
 factorial :: String
-factorial = "y:=x; z:=1; while y>1 do (z:=z*y; y:=y-1); y:=0"
+factorial = 
+    "   y := x;             \
+      \ z := 1;             \
+      \ while (y > 1) do    \
+      \     (z := z * y;    \
+      \      y := y - 1);   \
+      \ y := 0"
 
 -- Harness
 
